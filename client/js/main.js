92512f8faba9d154b33febdb4c1cc395442a6d3f
wangyichen1064431086 <1064431086@qq.com>
2017-09-04 16:58:24 +0800

    Set the mode to 'prod'

--- client/js/main.js
+++ client/js/main.js
@@ -2,7 +2,7 @@ import BannerImg from "./src/bannerImg.js";
 import BigImg from "./src/bigImg.js";
 import Switch from "./src/switch.js";
 
-const mode = "dev";
+const mode = "prod";
 
 const bannerObj = new BannerImg("bannerImgSection", mode);
 const bigObj = new BigImg("bigPicSection", mode);
